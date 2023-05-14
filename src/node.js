22021f9419afc7cd7ef70184f9f181695eb2d0c4
suke0930 <akitochuanshang@gmail.com>
2023-05-15 07:42:40 +0900

    は？

--- src/node.js
+++ src/node.js
@@ -8,7 +8,7 @@ const { exec } = require('child_process');
 const obs = new OBSWebSocket();
 let effecters = []//エフェクターを複数化するための機構
 const time = 60000 * 20//配信する時間
-
+const true = false
 const suke = false//sukeじゃないなら切れ！
 const config = JSON.parse(fs.readFileSync("./config/config.json", 'utf8').toString());
 //console.log(config)
